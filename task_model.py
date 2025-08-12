a3dafc39968bb1d529a69001ad390106787ce5cf
MehradMi <165255856+MehradMi@users.noreply.github.com>
2025-08-12 11:42:59 +0330

    Working on the task_model.py file

--- task_model.py
+++ task_model.py
@@ -10,8 +10,8 @@ class Task:
     character_id: Optional[int] = None
     task_title: str = ""
     task_description: str = ""
-    task_difficulty: str = "medium"     # easy, medium, hard, daunting (defaults to medium)
-    task_category: str = "general"      # TODO: It would be nice to be able add categories at will, must implement the mechanism for it
+    task_difficulty: str = None     # easy, medium, hard, daunting (defaults to medium)
+    task_category: str = None           # TODO: It would be nice to be able add categories at will, must implement the mechanism for it
     task_estimated_days: float = 1.0
     task_estimated_hours: float = 1.0
     task_actual_days: float = 0.0
@@ -33,7 +33,30 @@ class Task:
 
         return int(base_xp.get(self.task_difficulty, 50) * time_multiplier * class_bonus)
 
-    # TODO: There are some other methods left, I will implement them later
+    def set_task_title(self, given_task_title):
+        """Users must set a title (a.k.a name) for their task, otherwise the task won't be created"""
+        self.task_title = given_task_title        
+        
+
+    def set_task_description(self, given_task_description: str = ""):
+        """Users can set their task description, if user input not provided, task description defaults to: 'general'"""
+        self.task_description = given_task_description
+
+        
+    def set_task_category(self, given_task_category: str = "general"): 
+        """Users can set their task category, if user input not provided, task category defaults to: 'general'"""
+        self.task_category = given_task_category
+
+    def set_task_difficulty(self, given_task_difficulty: str = "medium"):
+        """Users can set their task diffculty, if user input not provided, task difficulty defaults to: 'medium'"""
+        self.task_difficulty = given_task_difficulty
+
+    def set_task_proiority(self, given_task_proiority: int = 1):
+        """Users can set their task proiority, if user input not provided, task proiority defaults to: 1"""
+        self.task_priority = given_task_proiority
+
+    
+        
 
     
 
