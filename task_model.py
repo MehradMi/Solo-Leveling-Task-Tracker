67ab643abbe75e58b46bb518c542a87e83a4e8c1
MehradMi <165255856+MehradMi@users.noreply.github.com>
2025-08-12 12:00:13 +0330

    Working on the task_model.py file

--- task_model.py
+++ task_model.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from typing import Optional
 from datetime import datetime
 from dataclasses import dataclass
@@ -6,18 +7,18 @@ from character_class import CharacterClass
 @dataclass
 class Task:
     """Task model"""
-    task_id: Optional[int] = None
+    task_unique_id: str = str(uuid4())
     character_id: Optional[int] = None
     task_title: str = ""
     task_description: str = ""
-    task_difficulty: str = None     # easy, medium, hard, daunting (defaults to medium)
-    task_category: str = None           # TODO: It would be nice to be able add categories at will, must implement the mechanism for it
-    task_estimated_days: float = 1.0
+    task_difficulty: str = None         # easy, medium, hard, daunting (defaults to medium)
+    task_category: str = None           
+    task_estimated_days: int = 0
     task_estimated_hours: float = 1.0
     task_actual_days: float = 0.0
     task_actual_hours: float = 0.0
-    task_xp_reward: int = 50           # TODO: It should dynamically change with respect to the task's difficulty and the estimated time
-    task_status: str = "pending"       # pending, in_progress, completed, failed
+    task_xp_reward: int = 50           
+    task_status: str = None             # pending, in_progress, completed, failed
     task_priority: int = 1
     task_created_at: Optional[datetime] = None
     task_started_at: Optional[datetime] = None
@@ -37,12 +38,10 @@ class Task:
         """Users must set a title (a.k.a name) for their task, otherwise the task won't be created"""
         self.task_title = given_task_title        
         
-
     def set_task_description(self, given_task_description: str = ""):
         """Users can set their task description, if user input not provided, task description defaults to: 'general'"""
         self.task_description = given_task_description
-
-        
+ 
     def set_task_category(self, given_task_category: str = "general"): 
         """Users can set their task category, if user input not provided, task category defaults to: 'general'"""
         self.task_category = given_task_category
@@ -51,11 +50,19 @@ class Task:
         """Users can set their task diffculty, if user input not provided, task difficulty defaults to: 'medium'"""
         self.task_difficulty = given_task_difficulty
 
+    def set_task_status(self, given_task_status: str = "pending"):
+        """Users can set their task status, if user input not provided, task status defaults to: 'pending'"""
+        self.task_status = given_task_status
+        # TODO: When the task status is set to completed or failed update the task_ended_at timer
+        # TODO: Once the task status is set to in_progress update the task_started_at timer
+
     def set_task_proiority(self, given_task_proiority: int = 1):
         """Users can set their task proiority, if user input not provided, task proiority defaults to: 1"""
         self.task_priority = given_task_proiority
 
-    
+    # TODO: Class method for setting the task_dead_line member variable
+
+    # TODO: Task forking mechanism (The game changer)    
         
 
     
